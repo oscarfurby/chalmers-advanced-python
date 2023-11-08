a78c1a4e94307426da0b0f25fe44a3764e7f8f76
oscarfurby <oscarfurby@hotmail.se>
2023-11-08 14:29:07 +0100

    Update ex03.py

--- exercises/ex03/ex03.py
+++ exercises/ex03/ex03.py
@@ -1,7 +1,11 @@
 ## Question 1: directory structure
 
 ## Question 2: graph representations
-adj_list = {0: [1, 3, 2], 1: [0, 2], 2: [1, 0, 3], 3: [2]}
+adj_list = {
+    0: [1, 3, 2], 
+    1: [0, 2], 
+    2: [1, 0, 3], 
+    3: [2]}
 
 def adj2mat(adj):
     n = len(adj)
@@ -33,13 +37,15 @@ print(mat2adj(adj2mat(adj_list), [0,1,2,3]))
 
 
 ## Question 3: equality between graphs
-def included(edges1, edges2):   #helper function
-    for (src, trg) in edges1:
-        if (not (src, trg) in edges2) and (not (trg, src) in edges2):
-            return False
-    return True
+
 
 def equal(edges1, edges2):
+    def included(edges1, edges2):   #helper function
+        for (src, trg) in edges1:
+            if (not (src, trg) in edges2) and (not (trg, src) in edges2):
+                return False
+        return True
+    
     if len(edges1) != len(edges2):
         return False
     if included(edges1, edges2) and included(edges2, edges1):
@@ -52,7 +58,7 @@ print(equal([(1, 2), (2, 3)], [(3, 2), (2, 2), (2,1)]))
 
 
 ## Question 4: depth-first search
-def bfs(graph, current_node, visit_complete=[]):
+def bfs(graph, current_node, visit_complete=[]): #breadth-first-search
     visit_complete.append(current_node)
     queue = []
     queue.append(current_node)
@@ -65,6 +71,8 @@ def bfs(graph, current_node, visit_complete=[]):
             if neighbour not in visit_complete:
                 visit_complete.append(neighbour)
                 queue.append(neighbour)
+            
+bfs(adj_list, 0)
 
 ''' PSEUDOCODE FROM WIKIPEDIA
     procedure DFS(G, v) is
@@ -74,7 +82,18 @@ def bfs(graph, current_node, visit_complete=[]):
                 recursively call DFS(G, w)
 '''
 
-def dfs():
-    pass
+def dfs(graph, current_node, visit_complete=[]): #depth-first-search
+    print(current_node)
+    visit_complete.append(current_node)
+    for neighbour in graph[current_node]:
+        if not neighbour in visit_complete:
+            dfs(graph, neighbour, visit_complete)
+
+dfs(adj_list, 3)
+
+#Bonus: implement dfs iteratively
+
 
 ## Question 5: multigraphs
+
+multi_edgelist = [(1, 2), (2, 3), (3, 2)]
